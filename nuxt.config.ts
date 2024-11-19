19e6c7b591cc4a88e7336f0f205354ef79a029cf
Andrew <a.thibs98@gmail.com>
2024-11-19 00:29:27 -0700

    uuuuuuu

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,33 +11,4 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/ui'
   ],
-  nitro: {
-    // Switch back to regular preset since edge is causing polyfill issues
-    preset: 'netlify',
-    externals: {
-      external: [
-        'nitropack',
-        'nitropa',
-        '_deno-env-polyfill'
-      ]
-    },
-    moduleSideEffects: ['unenv/runtime/polyfill/fetch.node'],
-  },
-
-  vite: {
-    build: {
-      rollupOptions: {
-        external: [
-          'nitropack',
-          'nitropa',
-          /^node:/,
-          /_deno-env-polyfill/,
-          /^netlify-(edge|lambda)/
-        ]
-      }
-    },
-    optimizeDeps: {
-      exclude: ['nitropack', 'nitropa']
-    }
-  },
 })
